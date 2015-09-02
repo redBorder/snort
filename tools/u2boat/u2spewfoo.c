0615cb4259e6b5c4c8bdb5411e9796bfdc3edc0a
Diego Fernández <bigomby@gmail.com>
2015-09-02 16:18:53 +0200

    Check if calloc was succesful.
    Conflicts:
    	tools/u2boat/u2spewfoo.c

--- tools/u2boat/u2spewfoo.c
+++ tools/u2boat/u2spewfoo.c
@@ -461,6 +461,25 @@ static void LogBuffer (const uint8_t* p, unsigned n,FILE *out_file)
     }
 }
 
+static void LogContinuousBuffer (const uint8_t* p, unsigned n,FILE *out_file)
+{
+    size_t len = n+1;
+    char * txt = (char *) calloc (len , sizeof(char));
+    if (txt != NULL) {
+        unsigned odx = 0, idx = 0;
+
+        for ( idx = 0; idx < n; idx++)
+        {
+            uint8_t byte = p[idx];
+            txt[odx++] = isprint(byte) ? byte : '.';
+        }
+
+        txt[odx] = '\0';
+        fprintf(out_file,"%s\n", txt);
+        free(txt);
+    }
+}
+
 static void packet_dump(const u2record *record,FILE *out_file) {
     uint32_t counter;
     uint8_t *field;
