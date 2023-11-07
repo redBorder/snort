e0e1c4fffa1102ff4fe5245e578900dca45a10d3
David Vanhoucke <dvanhoucke@redborder.com>
2023-11-07 08:23:09 +0000

    snort rhel7

--- rpm/rpm_buid.sh
+++ rpm/rpm_buid.sh
@@ -1,4 +1,4 @@
-rm -f /home/$USER/rpmbuild/SOURCES/snort-2.9.8.0.tar.gz
+rm -f /root/rpmbuild/SOURCES/snort-2.9.8.0.tar.gz
 ( cd .. && make distclean )
-( cd && tar cfzv /home/$USER/rpmbuild/SOURCES/snort-2.9.8.0.tar.gz snort-2.9.8.0/ )
+( cd /root/projects && tar cfzv /root/rpmbuild/SOURCES/snort-2.9.8.0.tar.gz snort-2.9.8.0/ )
 rpmbuild -ba --target x86_64 snort.spec
